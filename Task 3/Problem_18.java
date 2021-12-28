02d0953cc2bc792a25fc5680efd39e2e75d06991
Ahmed Kamel <85279324+AhmedKamel188@users.noreply.github.com>
2021-12-28 06:40:14 +0200

    Updated

--- Task 3/Problem_18.java
+++ Task 3/Problem_18.java
@@ -1,63 +1,72 @@
 import java.util.Scanner;
 public class Problem_18{
 	public static void main(String[] args) {
-	    
-	    // receive the numbers as string line
-		System.out.print("Enter 10 numbers : ");
-		String numbers = new Scanner(System.in).nextLine();
 		
-		// split string into string array
-		String[] str = numbers.split(" ");
+		// First way 
+		// Create Array (Size = 10)
+		int[] numbers = new int[10];
 		
-		// convert string array into integers array
+		// Receive 10 numbers from user
+		for(int i = 0; i < 10; i++){
+			System.out.print("Enter number " + (i+1) + " : ");
+			numbers[i] = new Scanner(System.in).nextInt();
+		}
+		
+		// Call eliminateDuplicates method
+		int[] new_numbers = eliminateDuplicates(numbers);
+		
+		// Print the eliminated array (new numbers)
+		System.out.print("The distinct numbers : ");
+		for (int i = 0; i < 10; i++){
+			if(new_numbers[i] != 0)					// To only avoid printing ZEROs
+			System.out.print(new_numbers[i] + " ");
+		}
+		
+		// Second way 
+		// Receive the numbers as string line
+		System.out.print("\nEnter 10 numbers : ");
+		String numbers_str = new Scanner(System.in).nextLine();
+		
+		// Split string into SubStrings array
+		String[] str = numbers_str.split(" ");
+		
+		// Convert string array into integers array
 		int[] nums = new int[10];
 		for (int i = 0; i < 10; i++){
 		    nums[i] = Integer.parseInt(str[i]);
 		}
 		
-		// call function for integers array
+		// Call function for integers array
 		int[] eliminated = eliminateDuplicates(nums);
 		
-		// get index of last element
-		int index = 0;
-		for (int i = 0; i < 10; i++){
-		    if (eliminated[i] == eliminated[i+1]){
-		        break;
-		    }
-		    else{
-		        index++;
-		    }
-		}
-		
-		// print the eliminated array
+		// Print the eliminated array (new numbers)
 		System.out.print("The distinct numbers : ");
-		for (int i = 0; i < index; i++) System.out.print(eliminated[i] + " ");
-		
+		for (int i = 0; i < 10; i++){
+			if(new_numbers[i] != 0)					// To only avoid printing ZEROs
+			System.out.print(new_numbers[i] + " ");
+		}
 	}
 	
 	public static int[] eliminateDuplicates(int[] list){
 	    
-	    int[] eliminated = new int[10];
+	    int[] eliminated = new int[list.length];
 	    int index = 0;
 	    boolean flag = false;
 	    
-	    for (int i = 0; i < list.length; i++){
-	        for (int j = 0; j < 10; j++){
-	            if (list[i] == eliminated[j]){
+	    for (int i = 0; i < list.length; i++){   	// Loop on the non-eliminated array
+	        for (int j = 0; j < 10; j++){  			// Loop on eliminated array
+	            if (list[i] == eliminated[j]){		// Check if number from non-eliminated is found in eliminated
 	                flag = true;
 	                break;
 	            }
 	        }
 	        
-	        if (!flag){
+	        if (!flag){								// If not found then add the number to eliminated
 	            eliminated[index++] = list[i];
-	        }
-	        else{
+	        } else{									// If found don't add
 	            flag = false;
-	            continue;
 	        }
 	    }
-	    
 	    return eliminated;
 	}
 }
